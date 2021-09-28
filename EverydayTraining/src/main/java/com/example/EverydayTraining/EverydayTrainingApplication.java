05ef4363ff47cbc40be725114584a46ea372c96c
Kimjoongyeon <jjjr69@naver.com>
2021-09-29 01:48:10 +0900

    second commit

---
+++ EverydayTraining/src/main/java/com/example/EverydayTraining/EverydayTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.EverydayTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydayTrainingApplication.class, args);
+	}
+
+}
---
+++ EverydayTraining/src/test/java/com/example/EverydayTraining/EverydayTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EverydayTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
