4765fa98d115a5b851af29bf26d54e3d29646bdb
Kimjoongyeon <jjjr69@naver.com>
2021-10-15 18:16:19 +0900

    네번쨰 커밋

--- frontend/src/layouts/Footer.vue
+++ frontend/src/layouts/Footer.vue
@@ -1,70 +1,69 @@
 <template>
-    <div>
-        <footer align="center">
-            <v-card flat tile class="white white--text text-center">
-                <v-card-text class="black--text pt-0">
-                    회사소개 | 채용정보 | 이용약관 | 개인정보 처리방침 | 위치기반 서비스 이용약관 | 중개사 사이트 바로가기
-                </v-card-text>
-                    
-                <v-card-text text-align="center" class="black--text pt-0">
-                    상호 : ooo  |  대표 : ooo  |  사업자등록번호: 111-11-11111<br>
-                    주소 : 서울특별시 서초구 서초대로 411, 5층(서초동, GT타워) (우 : 06615)<br>
-                    팩스 : 02-568-4908  | 통신판매업 신고번호 : 제2020-서울서초-0852호<br>
-                    서비스 이용문의 : 5959-2222  |  이메일 : 1111111  |  서비스제휴문의 : partnership@zigbang.com
-                </v-card-text>
+	<div>
+		<footer align="center">
+			<v-card flat tile class="white white--text text-center">
+				<v-card-text class="black--text pt-0">
+					회사소개 | 채용정보 | 이용약관 | 개인정보 처리방침 | 위치기반 서비스 이용약관 | 중개사 사이트 바로가기
+				</v-card-text>
 
-                <v-card-text>
-                    <v-btn v-for="icon in icons" :key="icon" class="mx-4 black--text blue--background" icon>
-                    <v-icon size="24px">
-                        {{ icon }}
-                    </v-icon>
-                    </v-btn>
-                </v-card-text>
-                <v-card-text class="black--text">
-                    <strong>Copyright © 대충회사이름. All Rights Reserved.</strong> 
-                </v-card-text>
-            </v-card>
-        </footer>
-    </div>
+				<v-card-text text-align="center" class="black--text pt-0">
+					상호 : ooo  |  대표 : ooo  |  사업자등록번호: 111-11-11111<br>
+					주소 : 서울특별시 서초구 서초대로 411, 5층(서초동, GT타워) (우 : 06615)<br>
+					팩스 : 02-568-4908  | 통신판매업 신고번호 : 제2020-서울서초-0852호<br>
+					서비스 이용문의 : 5959-2222  |  이메일 : 1111111  |  서비스제휴문의 : partnership@zigbang.com
+				</v-card-text>
+
+				<v-card-text>
+					<v-btn v-for="icon in icons" :key="icon" class="mx-4 black--text blue--background" icon>
+						<v-icon size="24px">
+							{{ icon }}
+						</v-icon>
+					</v-btn>
+				</v-card-text>
+				<v-card-text class="black--text">
+					<strong>Copyright © 대충회사이름. All Rights Reserved.</strong>
+				</v-card-text>
+			</v-card>
+		</footer>
+	</div>
 </template>
 
 <script>
-
-export default {
+  export default {
     name: 'Footer',
-      
+
     data() {
-        return {
-            icons: [
-                'mdi-facebook',
-                'mdi-twitter',
-                'mdi-linkedin',
-                'mdi-instagram',
-            ],
-            item: [
-                require('@/assets/img/Interior.jpg'),
-            ],
-                /*
-                {
-                icon: 'mdi-facebook',
-                //text: '메인페이지',
-                name: 'main_page',
-                route: '/'
-                },
-                {
-                icon: 'mdi-facebook',
-                //text: '프로젝트 리스트',
-                name: 'game_List',
-                route: '/'
-                },
-                {
-                icon: 'mdi-facebook',
-                //text: '뉴스',
-                name: 'daum_Crawl',
-                route: '/'
-                }
-            ]
-            */
+      return {
+        icons: [
+          'mdi-facebook',
+          'mdi-twitter',
+          'mdi-linkedin',
+          'mdi-instagram',
+        ],
+        // item: [
+        //   require('@/assets/img/Interior.jpg'),
+        // ],
+        /*
+				{
+				icon: 'mdi-facebook',
+				//text: '메인페이지',
+				name: 'main_page',
+				route: '/'
+				},
+				{
+				icon: 'mdi-facebook',
+				//text: '프로젝트 리스트',
+				name: 'game_List',
+				route: '/'
+				},
+				{
+				icon: 'mdi-facebook',
+				//text: '뉴스',
+				name: 'daum_Crawl',
+				route: '/'
+				}
+		]
+		*/
         /*
           isLogin: false,
           nav_drawer: false,
@@ -77,56 +76,54 @@ export default {
                 },
             ],
         */
-        
-        }
+
+      }
     },
     mounted () {
-        /*
-        // this.fetchSession()
-        this.$store.state.session = this.$cookies.get("user")
-        if (this.$store.state.session != null) {
-            this.isLogin = true
-        }
-        */
+      /*
+			// this.fetchSession()
+			this.$store.state.session = this.$cookies.get("user")
+			if (this.$store.state.session != null) {
+					this.isLogin = true
+			}
+			*/
     },
     computed: {
-        },
+    },
     methods: {
-        /*
-            logout () {
-                this.$cookies.remove("user")
-                this.isLogin = false
-                this.$store.state.session = null
-                alert('로그아웃 처리되었습니다 ')
-                this.$router.go();
-            },
-            fundingUpload() {
-                this.isLogin = true
-            },
-            fundingUploadFail() {
-                this.isLogin = false
-                alert('로그인이 필요합니다')
-                this.$router.push("login")
-            }
-            */
-        },
+      /*
+					logout () {
+							this.$cookies.remove("user")
+							this.isLogin = false
+							this.$store.state.session = null
+							alert('로그아웃 처리되었습니다 ')
+							this.$router.go();
+					},
+					fundingUpload() {
+							this.isLogin = true
+					},
+					fundingUploadFail() {
+							this.isLogin = false
+							alert('로그인이 필요합니다')
+							this.$router.push("login")
+					}
+					*/
+    },
     watch: {
-        /*
-        group () {
-            this.nav_drawer = false
-        }
-        */
-    }    
-}
+      /*
+			group () {
+					this.nav_drawer = false
+			}
+			*/
+    }
+  }
 </script>
 
-<style>
+<style scoped>
 
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
 
-* {
-    font-family: 'Noto Sans KR', sans-serif;
-}
-
-
+	* {
+		font-family: 'Noto Sans KR', sans-serif;
+	}
 </style>
--- frontend/src/layouts/Header.vue
+++ frontend/src/layouts/Header.vue
@@ -1,161 +1,233 @@
 <template>
-    <div>
-        <header>
-            <v-banner>
-                <v-toolbar  color="white">
-                <div id="logo">
-                    <a href="main"><img :src="require(`@/assets/img/logo.jpg`)"></a>
-                </div>
+	<header>
+		<div class="inner">
+			<h1 class="logo">
+				<img src="@/assets/img/Interior.jpg">
+			</h1>
 
-                <ul id="house-list">
-                    <!--<v-card width="108px" height="43px">-->
-                    <a font-color="black" >
-                        <span text text-align="left" class="mb-12" color="black" height="26px" width="108px" >
-                            아파트<br>
-                        <span-small text-align="left">매매/전월세/신축분양</span-small>
-                        </span>
-                    </a>
-                    <!--</v-card>-->
+			<ul class="navbar">
+				<li>
+					<a href="">
+						<span>아파트</span>
+						<span>매매/전월세/신축분양</span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span>빌라, 투룸+</span>
+						<span>신축분양/매매/전월세</span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span>원룸</span>
+						<span>전월세</span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<span>오피스텔</span>
+						<span>도시형생활주택/전월세</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</header>
 
-                    <a>
-                    <span text text-align="left" color="black" height="26px" width="108" position="#FA880B">빌라, 투룸+<br>
-                    <span-small text-align="left">신축분양/매매/전월세</span-small></span>
-                    </a>
-                    <a>
-                    <span text text-align="left" color="black" height="26px" width="33px">원룸<br>
-                    <span-small text-align="left">전월세</span-small></span>
-                    </a>
-                    <a>
-                    <span text text-align="left" color="black" height="26px" width="115px">오피스텔<br>
-                    <span-small text-align="left">도시형생활주택/전월세</span-small></span>
-                    </a>
-                </ul>
+<!--		<header>-->
+<!--			<v-banner>-->
+<!--				<v-toolbar  color="white">-->
+<!--					<div id="logo">-->
+<!--						<a href="main"><img src="src/assets/noimage.jpg"></a>-->
+<!--					</div>-->
 
-                <v-spacer></v-spacer>
+<!--					<ul id="house-list">-->
+<!--						&lt;!&ndash;<v-card width="108px" height="43px">&ndash;&gt;-->
+<!--						<a class=navMenu >-->
+<!--                        <span text text-align="left" class="mb-12" color="black" height="26px" width="108px" >-->
+<!--                            아파트<br>-->
+<!--                        <span-small text-align="left">매매/전월세/신축분양</span-small>-->
+<!--                        </span>-->
+<!--						</a>-->
+<!--						&lt;!&ndash;</v-card>&ndash;&gt;-->
 
-                <v-toolbar-items class="hidden-sm-and-down">
+<!--						<a class=navMenu>-->
+<!--                    <span text text-align="left" color="black" height="26px" width="108">빌라, 투룸+<br>-->
+<!--                    <span-small text-align="left">신축분양/매매/전월세</span-small></span>-->
+<!--						</a>-->
+<!--						<a class=navMenu>-->
+<!--                    <span text text-align="left" color="black" height="26px" width="33px">원룸<br>-->
+<!--                    <span-small text-align="left">전월세</span-small></span>-->
+<!--						</a>-->
+<!--						<a class=navMenu>-->
+<!--                    <span text text-align="left" color="black" height="26px" width="115px">오피스텔<br>-->
+<!--                    <span-small text-align="left">도시형생활주택/전월세</span-small></span>-->
+<!--						</a>-->
+<!--					</ul>-->
 
-                <v-btn text>
-                    로그인 및 회원가입
-                </v-btn>
+<!--					<v-spacer></v-spacer>-->
 
-               
+<!--					<v-toolbar-items class="hidden-sm-and-down">-->
 
-                <v-btn text>
-                    중개사무소 가입
-                </v-btn>
-                </v-toolbar-items>            
-                </v-toolbar>
-            </v-banner>
-        </header>
-    </div>
+<!--						<v-btn text>-->
+<!--							로그인 및 회원가입-->
+<!--						</v-btn>-->
+
+
+
+<!--						<v-btn text float="left" color="indigo" 	font-size= "16px"  font-weight= "400" >-->
+<!--							중개사무소 가입-->
+<!--						</v-btn>-->
+<!--					</v-toolbar-items>-->
+<!--				</v-toolbar>-->
+<!--			</v-banner>-->
+<!--		</header>-->
+<!--	</div>-->
 </template>
 
 <script>
-export default {
-    name: 'Header',
-      
-    data() {
-        return {
-                /*
-                {
-                icon: 'mdi-facebook',
-                //text: '메인페이지',
-                name: 'main_page',
-                route: '/'
-                },
-                {
-                icon: 'mdi-facebook',
-                //text: '프로젝트 리스트',
-                name: 'game_List',
-                route: '/'
-                },
-                {
-                icon: 'mdi-facebook',
-                //text: '뉴스',
-                name: 'daum_Crawl',
-                route: '/'
-                }
-            ]
-            */
-        /*
-          isLogin: false,
-          nav_drawer: false,
-          group: false,
-          loginLinks: [
-                {
-                text: '회원가입/로그인',
-                name: 'MemberLoginPage',
-                route: '/login'
-                },
-            ],
-        */
-        
-        }
-    },
-    mounted () {
-        /*
-        // this.fetchSession()
-        this.$store.state.session = this.$cookies.get("user")
-        if (this.$store.state.session != null) {
-            this.isLogin = true
-        }
-        */
-    },
-    computed: {
-        },
-    methods: {
-        /*
-            logout () {
-                this.$cookies.remove("user")
-                this.isLogin = false
-                this.$store.state.session = null
-                alert('로그아웃 처리되었습니다 ')
-                this.$router.go();
-            },
-            fundingUpload() {
-                this.isLogin = true
-            },
-            fundingUploadFail() {
-                this.isLogin = false
-                alert('로그인이 필요합니다')
-                this.$router.push("login")
-            }
-            */
-        },
-    watch: {
-        /*
-        group () {
-            this.nav_drawer = false
-        }
-        */
-    }    
-}
+  export default {
+    name: "Header"
+  }
 </script>
 
-<style>
+<style scoped>
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
 
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
+	* {
+		font-family: 'Noto Sans KR', sans-serif;
+		margin: 0 auto;
+		
+	}
 
-* {
-    font-family: 'Noto Sans KR', sans-serif;
-}
+	ol, ul {
+		list-style: none;
+	}
 
-#header {
-    max-height:"50px"; max-width:"112px";
-}
+	a {
+		color: #000;
+		text-decoration: none;
+	}
 
-#logo{
-    width: 122px;
-    height: 60px;
-    margin: 12px;
-    object-fit: cover; 
-    background-clip: border-box;
-}
 
-#house-list{
-    width: 660px;
-    height: 79px;
-    display: block;
-}
+
+	header {
+		max-height:100px;
+		/*max-width:112px;*/
+		padding: 15px 20px;
+		
+	}
+
+	header:after {
+		content: "";
+		display: block;
+		clear: both;
+	}
+
+	h1 {
+		float: left;
+		margin-right: 40px;
+	}
+
+	h1 > img {
+		width: 100px;
+	}
+
+	ul.navbar li {
+		float: left;
+		margin-right: 50px;
+		color: black;
+	}
+
+	ul.navbar li span {
+		display: block;
+		color: black;
+	}
+
+	ul.navbar li span:nth-child(1) {
+		font-weight: bold;
+		font-size: 20px;
+		color: black;
+	}
+
+	ul.navbar li span:nth-child(2) {
+		font-size: 12px;
+		color: black;
+	}
+
+
+	/*#logo{*/
+	/*	width: 122px;*/
+	/*	height: 60px;*/
+	/*	margin: 12px;*/
+	/*	object-fit: cover;*/
+	/*	background-clip: border-box;*/
+	/*}*/
+
+	/*#house-list{*/
+	/*	!* Font & Text *!*/
+	/*	font-size: 16px;*/
+	/*	font-style: normal;*/
+	/*	font-variant: normal;*/
+	/*	font-weight: 400;*/
+	/*	letter-spacing: normal;*/
+
+
+	/*	!* Color & Background *!*/
+	/*	background-attachment: scroll;*/
+	/*	background-color: rgba(0, 0, 0, 0);*/
+	/*	background-image: none;*/
+	/*	background-position: 0% 0%;*/
+	/*	background-repeat: repeat;*/
+	/*	color: rgb(0, 0, 0);*/
+
+	/*	!* Box *!*/
+	/*	height: 79px;*/
+	/*	width: 660.594px;*/
+	/*	margin: 0px;*/
+	/*	padding: 0px;*/
+	/*	max-height: none;*/
+	/*	min-height: 0px;*/
+	/*	max-width: none;*/
+	/*	min-width: 0px;*/
+
+	/*}*/
+
+	/*.navMenu {*/
+	/*	!* Font & Text *!*/
+	/*	font-size: 16px;*/
+	/*	font-style: normal;*/
+	/*	font-variant: normal;*/
+	/*	font-weight: 400;*/
+	/*	letter-spacing: normal;*/
+	/*	line-height: 18px;*/
+	/*	text-decoration: none solid rgb(0, 0, 0);*/
+	/*	text-align: left;*/
+	/*	text-indent: 0px;*/
+	/*	text-transform: none;*/
+	/*	white-space: normal;*/
+	/*	word-spacing: 0px;*/
+
+	/*	!* Color & Background *!*/
+	/*	background-attachment: scroll;*/
+	/*	background-color: rgba(0, 0, 0, 0);*/
+	/*	background-image: none;*/
+	/*	background-position: 0% 0%;*/
+	/*	background-repeat: repeat;*/
+	/*	color: rgb(0, 0, 0);*/
+
+	/*	!* Box *!*/
+	/*	height: 43px;*/
+	/*	width: 84.7969px;*/
+	/*	margin: 0px;*/
+	/*	padding: 19px 15px 17px;*/
+	/*	max-height: none;*/
+	/*	min-height: 0px;*/
+	/*	max-width: none;*/
+	/*	min-width: 0px;*/
+
+
+
+
+	/*}*/
 </style>
